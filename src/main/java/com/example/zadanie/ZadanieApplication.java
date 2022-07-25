fd7338924e7b4cc32bfeea12b0187817188690a4
Taialin <belichtaia@gmail.com>
2022-07-25 10:55:39 +0300

    ---

--- src/main/java/com/example/zadanie/ZadanieApplication.java
+++ src/main/java/com/example/zadanie/ZadanieApplication.java
@@ -3,16 +3,24 @@ package com.example.zadanie;
 import com.example.zadanie.repository.NewsRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-
+@Configuration
 public class ZadanieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZadanieApplication.class, args);
     }
 
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
